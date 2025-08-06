af8109b27649a23f6674aaf5b02519c481ee4f41
Ehsan Bavand <ehsan.bavand@ardene.com>
2025-08-06 13:58:44 -0400

    Add new update for login page

--- src/routes/AppRoutes.js
+++ src/routes/AppRoutes.js
@@ -1,19 +1,18 @@
-import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import LoginPage from "../pages/Login";
 import RegisterPage from "../pages/Register";
 import DashboardPage from "../pages/DashboardPage";
 
 const AppRoute = () => {
   return (
-    <Router>
-      <Routes>
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-        <Route path="/dashboard" element={<DashboardPage />} />
-        {/* Add other routes here later */}
-      </Routes>
-    </Router>
+    <Routes>
+      {/* ✅ Default route redirects to /login */}
+      <Route path="/" element={<Navigate to="/login" />} />
+
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/register" element={<RegisterPage />} />
+      <Route path="/dashboard" element={<DashboardPage />} />
+    </Routes>
   );
 };
 
